b04e417933b7f995edb42282b74688cb58796272
mhhofc12-mhh <mhhofc12@gmail.com>
2026-09-02 17:49:02 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,25 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-android { namespace="com.example.easyenglish"; compileSdk=35
-    defaultConfig { applicationId="com.example.easyenglish"; minSdk=23; targetSdk=35; versionCode=1; versionName="1.0" }
+
+android {
+    namespace = "com.example.easyenglish"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.easyenglish"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
